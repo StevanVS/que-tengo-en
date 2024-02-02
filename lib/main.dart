c9311f2bd580009297cc91be3c091d7b1c43a0c5
Stevan Velez <stevanvs@outlook.com>
2024-02-02 12:42:22 -0500

    Avance UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:que_tengo_en/ui/pages/lugares/lugares_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,26 +13,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Que tengo en',
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          useMaterial3: true,
+          colorSchemeSeed: Colors.amber),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Que tengo en'),
+          brightness: Brightness.light,
+          useMaterial3: true,
+          colorSchemeSeed: Colors.amber),
+      themeMode: ThemeMode.system,
+      home: const LugaresPage(),
     );
   }
 }
